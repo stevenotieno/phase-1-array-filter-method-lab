82cf2b783e396b03065eb5248d06d607121c4bfc
+ <+>
2022-12-05 16:44:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// Returns all drivers that match the passed in name even if case does not match but letters do
+let findMatching = (listOfDrivers, searchedDriver) => {
+    return listOfDrivers.filter( driverName => {  return driverName.toLowerCase() === searchedDriver.toLowerCase() });
+ }
+ 
+ // Returns a driver if beginning provided charaters match with passed matchingLetters
+ let fuzzyMatch = (listOfDrivers, matchingLetters) => {
+    return listOfDrivers.filter( driverName => {  return driverName.substring(0, 2) === matchingLetters });
+ }
+ 
+ // Accesses a data structure to check if name matches.
+ let matchName = (listOfDrivers, searchedDriver) => {
+    return listOfDrivers.filter( driverName => { return driverName.name === searchedDriver })
+ }
